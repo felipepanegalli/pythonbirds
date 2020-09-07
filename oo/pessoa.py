0c6ed161f8ec2b3c70ccbd1653931b3b1adae53e
Felipe <felipe.panegalli@gmail.com>
2020-09-07 16:36:56 -0300

    Criados tributos de instância nome e idade da pessoa.py

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,11 +1,18 @@
 # Pep8 CamelCase para classes
 class Pessoa:
+    def __init__(self, nome=None, idade=30):
+        self.nome = nome
+        self.idade = idade
+
     # Pep8 snack_case para functions
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Felipe', 30)
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Teste'
+    p.idade = 40
