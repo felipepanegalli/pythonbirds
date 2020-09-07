3aeffa64eb143e46dfd3b85450b7dc1d8a1dbb02
Felipe <felipe.panegalli@gmail.com>
2020-09-07 16:30:37 -0300

    Criada a finção da pessoa.py

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,3 +1,11 @@
 # Pep8 CamelCase para classes
 class Pessoa:
-    pass
+    # Pep8 snack_case para functions
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(id(p))
+    print(p.cumprimentar())
