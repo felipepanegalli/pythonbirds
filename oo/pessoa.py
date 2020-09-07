cc5c414907e8c5f77bc0e831b053f6185232e6e0
Felipe <felipe.panegalli@gmail.com>
2020-09-07 16:43:55 -0300

    Criado atributo complexo filho

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,9 @@
 # Pep8 CamelCase para classes
 class Pessoa:
-    def __init__(self, nome=None, idade=30):
+    def __init__(self, *filhos, nome=None, idade=30):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     # Pep8 snack_case para functions
     def cumprimentar(self):
@@ -10,9 +11,9 @@ class Pessoa:
 
 
 if __name__ == '__main__':
-    p = Pessoa('Felipe', 30)
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Teste'
-    p.idade = 40
+    renzo = Pessoa(nome='Renzo', idade=30)
+    luciano = Pessoa(renzo, nome='Luciano', idade=40)
+    print(luciano.nome)
+    print(luciano.idade)
+    for filhos in luciano.filhos:
+        print(filhos.nome)
